8ef4fdced8194a8c1ce6ba0fe1450e55e18b55df
damszew <damian.szewczyk111@gmail.com>
2020-12-31 12:18:15 +0100

    Use new path to docker-compose in query-service-ts tests

--- tests/query_service_ts/__init__.py
+++ tests/query_service_ts/__init__.py
@@ -10,9 +10,10 @@ from tests.common.query_service_ts import QueryServiceTs
 
 INPUT_PORT = '50104'
 
+
 @pytest.fixture
 def prepare_env():
-    with cdl_env('.', victoria_metrics_config=VictoriaMetricsConfig()) as env:
+    with cdl_env('../deployment/compose', victoria_metrics_config=VictoriaMetricsConfig()) as env:
         with QueryServiceTs(INPUT_PORT, env.victoria_metrics_config),\
                 grpc.insecure_channel(f"localhost:{INPUT_PORT}") as channel:
             stub = pb2_grpc.QueryServiceTsStub(channel)
