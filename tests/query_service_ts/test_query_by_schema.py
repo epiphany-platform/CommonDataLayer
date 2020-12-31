23e4812d191c58b5785c74299eb1f674e1ffb501
damszew <damian.szewczyk111@gmail.com>
2020-12-31 11:39:09 +0100

    Fix query_by_schema tests for qury-service-ts

--- tests/query_service_ts/test_query_by_schema.py
+++ tests/query_service_ts/test_query_by_schema.py
@@ -7,24 +7,30 @@ from tests.common import load_case
 from rpc.proto.query_service_ts_pb2 import SchemaId
 
 
-@pytest.fixture(params=["schema/single"])
+@pytest.fixture(params=["schema/single", "schema/non_existing", "schema/invalid_schema_id"])
 def prepare(request, prepare_env):
     db, stub = prepare_env
     data, expected = load_case(request.param, "query_service_ts")
     db.insert_test_data(data['database_setup'])
-    time.sleep(2)  # Ensure that 'search.latencyOffset' passed
 
     query = data["query_for"]
     return db, stub, expected, query
 
-# TODO: Handle that instant query returns current timestamp instead of timestamp of insert
-
 
 def test_query_by_schema(prepare):
     db, stub, expected, query = prepare
-    print(db.fetch_data_table())
+
     query_request = SchemaId(**query)
-    print(query_request)
     response = stub.QueryBySchema(query_request)
-    print(response)
-    assert json.loads(str(response.timeseries)) == expected
+
+    assert_without_timestamp(json.loads(str(response.timeseries)), expected)
+
+
+# Instant query returns current timestamp instead of timestamp of data insert
+def assert_without_timestamp(lhs, rhs):
+    try:
+        rhs['data']['result'][0]['value'][0] = lhs['data']['result'][0]['value'][0]
+    except IndexError:
+        pass
+
+    assert lhs == rhs
