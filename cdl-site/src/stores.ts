216e480204c9dddb91c423f40d616d4ccb90a627
Sam Mohr <sam@mohr.codes>
2021-01-25 15:48:14 -0500

    set default API URL

--- cdl-site/src/stores.ts
+++ cdl-site/src/stores.ts
@@ -1,4 +1,4 @@
-import { writable } from "svelte/store";
+import { get, writable } from "svelte/store";
 import { notLoaded, RemoteData, Schema } from "./models";
 
 export const schemas = writable<RemoteData<Schema[]>>(notLoaded);
@@ -8,6 +8,10 @@ export const apiUrl = writable(localStorage.getItem("api-url") || "");
 apiUrl.subscribe(url => {
   localStorage.setItem("api-url", url);
 });
+
+if (get(apiUrl) === "") {
+  apiUrl.set("http://localhost:50106");
+}
 
 export const darkMode = writable(
   localStorage.getItem("dark-mode") === "true",
