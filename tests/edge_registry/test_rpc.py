5e93be2a0fa2781c0f961b80161e15f810486396
Łukasz Biel <lukasz.p.biel@gmail.com>
2021-03-26 09:59:48 +0100

    Apply code formatting

--- tests/edge_registry/test_rpc.py
+++ tests/edge_registry/test_rpc.py
@@ -38,7 +38,9 @@ def prepare():
 def test_add_relation(prepare):
     parent_schema_id = "3fb03807-2c51-43c8-aa57-34f8d2fa0186"
     child_schema_id = "1d1cc7a5-9277-48bc-97d3-3d99cfb633dd"
-    resp = prepare.AddRelation(SchemaRelation(parent_schema_id=parent_schema_id, child_schema_id=child_schema_id))
+    resp = prepare.AddRelation(
+        SchemaRelation(parent_schema_id=parent_schema_id,
+                       child_schema_id=child_schema_id))
     relations = prepare.ListRelations(Empty())
 
     result = relations.items[0]
@@ -48,52 +50,92 @@ def test_add_relation(prepare):
     assert result.parent_schema_id == parent_schema_id
     assert result.child_schema_id == child_schema_id
 
+
 def test_get_relation(prepare):
     parent_schema_id = "3fb03807-2c51-43c8-aa58-3468d26a0186"
     child_schema_id = "1d1cc7a5-9277-48bc-97d3-3d99cfb633ac"
-    relation_id = prepare.AddRelation(SchemaRelation(parent_schema_id=parent_schema_id, child_schema_id=child_schema_id)).relation_id
+    relation_id = prepare.AddRelation(
+        SchemaRelation(parent_schema_id=parent_schema_id,
+                       child_schema_id=child_schema_id)).relation_id
 
-    result = prepare.GetRelation(RelationQuery(relation_id=relation_id, parent_schema_id=parent_schema_id)).child_schema_id
+    result = prepare.GetRelation(
+        RelationQuery(relation_id=relation_id,
+                      parent_schema_id=parent_schema_id)).child_schema_id
 
     assert child_schema_id == result
 
+
 def test_get_relations(prepare):
     parent_schema_id = "1d1cc7a5-9277-48bc-97d3-3d99cfb63300"
-    relation1 = prepare.AddRelation(SchemaRelation(parent_schema_id=parent_schema_id, child_schema_id="1d1cc7a5-9277-48bc-97d3-3d99cfb63301")).relation_id
-    relation2 = prepare.AddRelation(SchemaRelation(parent_schema_id=parent_schema_id, child_schema_id="1d1cc7a5-9277-48bc-97d3-3d99cfb63302")).relation_id
+    relation1 = prepare.AddRelation(
+        SchemaRelation(parent_schema_id=parent_schema_id,
+                       child_schema_id="1d1cc7a5-9277-48bc-97d3-3d99cfb63301")
+    ).relation_id
+    relation2 = prepare.AddRelation(
+        SchemaRelation(parent_schema_id=parent_schema_id,
+                       child_schema_id="1d1cc7a5-9277-48bc-97d3-3d99cfb63302")
+    ).relation_id
 
-    result = list(map(lambda x: x.relation_id, prepare.GetSchemaRelations(SchemaId(schema_id=parent_schema_id)).items))
+    result = list(
+        map(
+            lambda x: x.relation_id,
+            prepare.GetSchemaRelations(
+                SchemaId(schema_id=parent_schema_id)).items))
 
     assert [relation1, relation2] == result
+
 
 def test_add_get_edge(prepare):
     parent_schema_id = "1d1cc7a5-9277-48bc-97d3-3d99cfb63303"
     child_schema_id = "ed1cc7a5-9277-48bc-97d3-3d99cfb6330c"
-    relation = prepare.AddRelation(SchemaRelation(parent_schema_id=parent_schema_id, child_schema_id=child_schema_id)).relation_id
+    relation = prepare.AddRelation(
+        SchemaRelation(parent_schema_id=parent_schema_id,
+                       child_schema_id=child_schema_id)).relation_id
 
     parent_object_id = "1d1cc7a5-9277-48bc-97d3-3d99cfb633aa"
     child1 = "1d1cc7a5-9277-48bc-97d3-3d99cfb633a1"
     child2 = "1d1cc7a5-9277-48bc-97d3-3d99cfb633a2"
 
-    prepare.AddEdges(ObjectRelations(relations=[Edge(relation_id=relation, parent_object_id=parent_object_id, child_object_ids=[child1, child2])]))
+    prepare.AddEdges(
+        ObjectRelations(relations=[
+            Edge(relation_id=relation,
+                 parent_object_id=parent_object_id,
+                 child_object_ids=[child1, child2])
+        ]))
 
-    result = prepare.GetEdge(RelationIdQuery(relation_id=relation, parent_object_id=parent_object_id)).child_object_ids
+    result = prepare.GetEdge(
+        RelationIdQuery(relation_id=relation,
+                        parent_object_id=parent_object_id)).child_object_ids
 
     assert result == [child1, child2]
 
+
 def test_get_edges(prepare):
-    relation1 = prepare.AddRelation(SchemaRelation(parent_schema_id="1d1cc7a5-9277-48bc-97d3-3d99cfb63000", child_schema_id="1d1cc7a5-9277-48bc-97d3-3d99cfb63001")).relation_id
-    relation2 = prepare.AddRelation(SchemaRelation(parent_schema_id="1d1cc7a5-9277-48bc-97d3-3d99cfb6300c", child_schema_id="1d1cc7a5-9277-48bc-97d3-3d99cfb6300a")).relation_id
+    relation1 = prepare.AddRelation(
+        SchemaRelation(parent_schema_id="1d1cc7a5-9277-48bc-97d3-3d99cfb63000",
+                       child_schema_id="1d1cc7a5-9277-48bc-97d3-3d99cfb63001")
+    ).relation_id
+    relation2 = prepare.AddRelation(
+        SchemaRelation(parent_schema_id="1d1cc7a5-9277-48bc-97d3-3d99cfb6300c",
+                       child_schema_id="1d1cc7a5-9277-48bc-97d3-3d99cfb6300a")
+    ).relation_id
 
     parent = "1d1cc7a5-9277-48bc-97d3-3d99cfb63002"
     child1 = "1d1cc7a5-9277-48bc-97d3-3d99cfb63003"
     child2 = "1d1cc7a5-9277-48bc-97d3-3d99cfb63005"
 
-    prepare.AddEdges(ObjectRelations(relations=[
-        Edge(relation_id=relation1, parent_object_id=parent, child_object_ids=[child1]),
-        Edge(relation_id=relation2, parent_object_id=parent, child_object_ids=[child2])
-    ]))
+    prepare.AddEdges(
+        ObjectRelations(relations=[
+            Edge(relation_id=relation1,
+                 parent_object_id=parent,
+                 child_object_ids=[child1]),
+            Edge(relation_id=relation2,
+                 parent_object_id=parent,
+                 child_object_ids=[child2])
+        ]))
 
-    result = list(map(lambda x: (x.relation_id, x.child_object_ids), prepare.GetEdges(ObjectIdQuery(object_id=parent)).relations))
+    result = list(
+        map(lambda x: (x.relation_id, x.child_object_ids),
+            prepare.GetEdges(ObjectIdQuery(object_id=parent)).relations))
 
     assert [(relation1, [child1]), (relation2, [child2])] == result
