d5d72c9353bbf69d6cd9884ca287d4a0d1cc3eaf
Wojciech Polak <wojciech.polak@hitachi-powergrids.com>
2021-04-14 12:24:56 +0200

    feat: Open Telemetry

--- crates/utils/src/tracing.rs
+++ crates/utils/src/tracing.rs
@@ -1,3 +1,25 @@
+use opentelemetry::global;
+use opentelemetry::sdk::propagation::TraceContextPropagator;
+use tracing_subscriber::prelude::*;
+
+pub mod grpc;
+pub mod kafka; // Used mostly in common publisher and [parallel_]consumer
+
 pub fn init() {
-    tracing_subscriber::fmt::init();
+    global::set_text_map_propagator(TraceContextPropagator::new());
+
+    let tracer = opentelemetry_jaeger::new_pipeline()
+        .install_batch(opentelemetry::runtime::Tokio)
+        .unwrap();
+
+    let opentelemetry = tracing_opentelemetry::layer().with_tracer(tracer);
+
+    let fmt = tracing_subscriber::fmt::layer();
+
+    tracing_subscriber::registry()
+        .with(tracing_subscriber::EnvFilter::from_default_env())
+        .with(fmt)
+        .with(opentelemetry)
+        .try_init()
+        .unwrap();
 }
