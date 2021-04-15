f930f30919e142ee29a02b106e51bacbb60c7ad0
Wojciech Polak <wojciech.polak@hitachi-powergrids.com>
2021-04-15 12:06:55 +0200

    fix: Check if tokio runtime is running

--- crates/utils/src/tracing.rs
+++ crates/utils/src/tracing.rs
@@ -1,5 +1,6 @@
 use opentelemetry::global;
 use opentelemetry::sdk::propagation::TraceContextPropagator;
+use tokio::runtime::Handle;
 use tracing_subscriber::prelude::*;
 
 pub mod grpc;
@@ -8,10 +9,14 @@ pub mod kafka; // Used mostly in common publisher and [parallel_]consumer
 pub fn init() {
     global::set_text_map_propagator(TraceContextPropagator::new());
 
-    let opentelemetry = opentelemetry_jaeger::new_pipeline()
-        .install_batch(opentelemetry::runtime::Tokio)
-        .map(|tracer| tracing_opentelemetry::layer().with_tracer(tracer))
-        .ok();
+    let opentelemetry = Handle::try_current()
+        .ok() // Check if Tokio runtime exists
+        .and_then(|_| {
+            opentelemetry_jaeger::new_pipeline()
+                .install_batch(opentelemetry::runtime::Tokio)
+                .ok()
+        })
+        .map(|tracer| tracing_opentelemetry::layer().with_tracer(tracer));
 
     let fmt = tracing_subscriber::fmt::layer();
 
