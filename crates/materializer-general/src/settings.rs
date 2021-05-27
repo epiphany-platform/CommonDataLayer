d1729ba00e70d82e484a2c9ca169f88ac5bec0c2
Łukasz Biel <lukasz.p.biel@gmail.com>
2021-05-27 12:54:07 +0200

    WIP

--- crates/materializer-general/src/settings.rs
+++ crates/materializer-general/src/settings.rs
@@ -1,13 +1,29 @@
+use communication_utils::publisher::CommonPublisher;
 use serde::Deserialize;
 use settings_utils::{LogSettings, MonitoringSettings, PostgresSettings};
+use utils::notification::NotificationSettings;
 
 #[derive(Debug, Deserialize)]
 pub struct Settings {
     pub input_port: u16,
 
     pub postgres: PostgresSettings,
+    pub kafka: KafkaProducerSettings,
+    #[serde(default)]
+    pub notifications: NotificationSettings,
 
     pub monitoring: MonitoringSettings,
 
     pub log: LogSettings,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct KafkaProducerSettings {
+    pub brokers: String,
+}
+
+impl Settings {
+    pub async fn publisher(&self) -> anyhow::Result<CommonPublisher> {
+        Ok(CommonPublisher::new_kafka(&self.kafka.brokers).await?)
+    }
 }
