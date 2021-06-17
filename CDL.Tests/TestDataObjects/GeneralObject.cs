e48c75c20c5268292a401ff5eea3d86cf0d6fd64
Przemyslaw Grzywacz <przemyslaw.grzywacz@hitachi-powergrids.com>
2021-06-17 16:57:32 +0200

    Add data generator

--- CDL.Tests/TestDataObjects/GeneralObject.cs
+++ CDL.Tests/TestDataObjects/GeneralObject.cs
@@ -6,42 +6,13 @@ namespace CDL.Tests.TestDataObjects
 {
     public class GeneralObject
     {
-        public string Id { get; set; }
+        public int Id { get; set; }
         public Person Person { get; set; }
-        public IList<object> Properties { get; set; }
-
-        public GeneralObject(int familyNumber = 1, int carNumber = 1, int propertyNumber = 1)
-        {
-            SetupObject();
-            Id = _fixture.Create<string>();
-            Person = _fixture.Create<Person>();
-            for (int i = 0; i < familyNumber; i++)
-            {
-                Properties.Add(_fixture.Create<FamilyPerson>());
-            }
-
-            for (int i = 0; i < carNumber; i++)
-            {
-                Properties.Add(_fixture.Create<Car>());
-            }
-
-            for (int i = 0; i < propertyNumber; i++)
-            {
-                Properties.Add(_fixture.Create<Property>());
-            }
-        }
-
+        public IList<Property> Properties { get; set; }
+        
         public string ToJSONString() 
         {
             return JsonSerializer.Serialize<GeneralObject>(this);
         }
-
-        private Fixture _fixture;
-        private void SetupObject()
-        {
-            _fixture = new Fixture();
-            Properties = new List<object>();
-        }
-
     }
 }
