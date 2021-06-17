bb46018bff826fd396cf54a19f6f1b69909ad585
Przemyslaw Grzywacz <przemyslaw.grzywacz@hitachi-powergrids.com>
2021-06-17 16:57:20 +0200

    Add data generator

---
+++ CDL.Tests/TestDataObjects/Person.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace CDL.Tests.TestDataObjects
+{
+    public class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public Gander Gander { get; set; }
+        public DateTime Birthday { get; set; }
+    }
+
+    public enum Gander
+    {
+        Male,
+        Female
+    }
+}
