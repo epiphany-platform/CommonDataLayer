24b4184e3bc7d0c9d5cef4ed93b57b3fbd408d8a
kononnable <kononnable@gmail.com>
2021-06-28 12:03:00 +0200

    simplify conditional compilation

--- e2e/src/lib.rs
+++ e2e/src/lib.rs
@@ -1,13 +1,9 @@
-#[cfg(all(test, feature = "e2e"))]
+#![cfg(all(test, feature = "e2e"))]
 mod api;
-#[cfg(all(test, feature = "e2e"))]
 mod object_builder;
 
-#[cfg(all(test, feature = "e2e"))]
 const POSTGRES_QUERY_ADDR: &str = "http://cdl-postgres-query-service:6400";
-#[cfg(all(test, feature = "e2e"))]
 const POSTGRES_INSERT_DESTINATION: &str = "cdl.document.data";
-#[cfg(all(test, feature = "e2e"))]
 const POSTGRES_MATERIALIZER_ADDR: &str = "http://cdl-postgres-materializer-general:6400";
-#[cfg(all(test, feature = "e2e"))]
+
 const GRAPHQL_ADDR: &str = "http://cdl-api:6402/graphql";
