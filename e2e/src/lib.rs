43555aa79de87b3961fdf561e5cb88550743568a
kononnable <kononnable@gmail.com>
2021-06-28 11:19:51 +0200

    feat: hide e2e tests under feature flag

--- e2e/src/lib.rs
+++ e2e/src/lib.rs
@@ -1,13 +1,13 @@
-#[cfg(test)]
+#[cfg(all(test, feature = "e2e"))]
 mod api;
-#[cfg(test)]
+#[cfg(all(test, feature = "e2e"))]
 mod object_builder;
 
-#[cfg(test)]
+#[cfg(all(test, feature = "e2e"))]
 const POSTGRES_QUERY_ADDR: &str = "http://cdl-postgres-query-service:6400";
-#[cfg(test)]
+#[cfg(all(test, feature = "e2e"))]
 const POSTGRES_INSERT_DESTINATION: &str = "cdl.document.data";
-#[cfg(test)]
+#[cfg(all(test, feature = "e2e"))]
 const POSTGRES_MATERIALIZER_ADDR: &str = "http://cdl-postgres-materializer-general:6400";
-#[cfg(test)]
+#[cfg(all(test, feature = "e2e"))]
 const GRAPHQL_ADDR: &str = "http://cdl-api:6402/graphql";
